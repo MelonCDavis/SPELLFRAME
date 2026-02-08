80687f81ffa1fbaa47dc092576141249cef17f70
Chris “Melon” Davis <209820108+MelonCDavis@users.noreply.github.com>
2026-02-08 16:59:32 -0500

    fix: fixing content

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -92,7 +92,7 @@ export default function ProfilePage() {
     }
 
   return (
-    <div className="relative profile-page-root flex flex-col pt-27">
+    <div className="fixed inset-0 profile-page-root flex flex-col">
       <div className="flex-1 rounded-2xl bg-linear-to-b from-neutral-500 via-neutral-600 to-neutral-800">
         <div className="w-full max-w-6xl mx-auto px-3 sm:px-4 py-6 space-y-6">
         {/* ========================= */}
