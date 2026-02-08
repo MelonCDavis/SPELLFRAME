2e07bc6ceafc1b883464b4d25c77393a4264e092
Chris “Melon” Davis <209820108+MelonCDavis@users.noreply.github.com>
2026-02-08 14:37:46 -0500

    removed elements and restructured the footer for better UX

--- frontend/src/components/layout/GlobalFooter.jsx
+++ frontend/src/components/layout/GlobalFooter.jsx
@@ -2,52 +2,25 @@ export default function GlobalFooter() {
     const year = new Date().getFullYear();
     return (
         <footer
-            className="
-              w-full
-              border-t border-Neutreal-800
-              bg-Neutreal-950
-              text-neutral-400
-              text-sm
-              "
+          className="
+            w-full
+            border-t border-Neutreal-800
+            bg-Neutreal-950
+            text-neutral-400
+            text-sm
+          "
         >
-            <div 
-                className="
-                    max-w-7xl 
-                    mx-auto 
-                    px-4 
-                    py-3 
-                    flex 
-                    flex-col
-                    sm:flex-row 
-                    justify-between 
-                    items-center
-                    gap-2
-                "
-            >
-                {/* Left */}
-                <div>
-                    &copy; {year} Melon Davis -- SPELLFRAME All rights reserved.
-                </div>
-                {/* Right */}
-                <div className= "flex gap-4 text-neutral-500">
-                    <a 
-                      href="https://github.com/MelonCDavis/SPELLFRAME"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="hover:text-neutral-200 transition"
-                    >
-                        GitHub
-                    </a>
-                    <a
-                      href="https://spellframe-rho.vercel.app/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="hover:text-neutral-200 transition"
-                    >
-                        Live App
-                    </a>
-                </div>
-            </div>
+          <div 
+            className="
+              max-w-7xl 
+              mx-auto 
+              px-4 
+              py-3 
+              text-center
+            "
+          >
+            &copy; {year} Melon Davis -- SPELLFRAME All rights reserved.
+          </div>
         </footer>
     )
 }
